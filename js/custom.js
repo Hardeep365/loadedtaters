0d6e92e9b0c18d2c735d680de88533bd854d3fb5
Hardeep365 <hardeepmonu96@gmail.com>
2024-09-29 21:36:34 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,14 @@
-
+jQuery(window).scroll(function() {  
+if (jQuery(this).scrollTop() > 0) {    
+    jQuery('header').addClass("sticky");  
+  } else {  
+    jQuery('header').removeClass("sticky");  
+  }  
+});
+jQuery(document).ready(function(){
+    jQuery("#toggle").click(function() {
+        jQuery(this).toggleClass("on");
+        jQuery("#menu").slideToggle();
+      });
+    });
+  
